4504ac372436e5a56c0bc40619d648d72a0d948d
Jim <33234558+jimb-cc@users.noreply.github.com>
2021-12-01 12:42:31 +0000

    created cubes

--- Assets/Scenes/aoc1a/aoc1a.cs
+++ Assets/Scenes/aoc1a/aoc1a.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 public class aoc1a : MonoBehaviour
 {
     public int deeperCount = 0;
+    public GameObject Cell;
+
     // Start is called before the first frame update
     void Start()
     {
+
+    
+
+
     string[] reader = System.IO.File.ReadAllLines( "Assets/Scenes/aoc1a/input.txt" );
         for ( int i = 0; i < reader.Length; i++ )
         {
             if (i>0){
                 Debug.Log( "index is " + i + ". The data is " + reader[i]  + " the previous point was " + reader[i-1] + "\n");
 
-
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.position = new Vector3(1.1f*i, (int.Parse(reader[i])/10)*-1, 0);
+                cube.GetComponent<MeshRenderer>().material.color = Color.yellow;
                 //is this value greater than the previous value?
                 if (int.Parse(reader[i])>int.Parse(reader[i-1]))
                 {
                     deeperCount++;
                     Debug.Log("Deeper!\n");
+                    cube.GetComponent<MeshRenderer>().material.color = Color.blue;
                 }
             }
         }
@@ -31,4 +40,7 @@ public class aoc1a : MonoBehaviour
     {
         
     }
+
+
+
 }
